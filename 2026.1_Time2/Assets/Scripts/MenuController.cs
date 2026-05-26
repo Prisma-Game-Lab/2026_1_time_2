493645070786e916c01ac462ef982030850dc44b
udagla5 <carloseduardopbernardo.ismart@gmail.com>
2026-05-25 23:25:17 -0300

    Menu com Config (Protótipo)

--- 2026.1_Time2/Assets/Scripts/MenuController.cs
+++ 2026.1_Time2/Assets/Scripts/MenuController.cs
@@ -3,11 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] private GameObject Menu;
+    [SerializeField] private GameObject Config; 
+
     public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
     }
 
+    public void OpenSettings()
+    {
+        Menu.SetActive(false);
+        Config.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        Config.SetActive(false);
+        Menu.SetActive(true);
+    }
+    
     public void QuitGame()
     {
         Application.Quit();
