f812e93d52ad979c7f31e7bab46aed50698fac17
udagla5 <carloseduardopbernardo.ismart@gmail.com>
2026-06-23 01:47:37 -0300

    Revert "Bug fix playtest1 v1 + Fundo floresta Lorenzo"
    This reverts commit 40eb62b4a94ef6db542a968c80f976d42edca9f9.

--- 2026.1_Time2/Assets/Scripts/Player/Player.cs
+++ 2026.1_Time2/Assets/Scripts/Player/Player.cs
@@ -7,15 +7,6 @@ public class Player : MonoBehaviour
     public int maxHealth = 3;
     private int currentHealth;
     public int takenDamage = 1;
-
-    /* O tal do invencibility frame*/
-    public float invincibleTime = 3.0f;
-
-    private bool isInvincible = false;
-
-    private float invincibleCurrentTime = 0.0f;
-
-
     public GameObject[] coracoes;
 
     [Header("Speed e Dash")]
@@ -51,14 +42,8 @@ public class Player : MonoBehaviour
             RotateTowardsMouse();
             SelectWeapon();
             PlayerDash();
-            if(isInvincible)
-            {
-                UpdateInvencibility();
-            }
         }
-        Debug.Log("Invincible: " + isInvincible);
     }
-
     void FixedUpdate()
     {
         // Movimentação do Player
@@ -76,16 +61,11 @@ public class Player : MonoBehaviour
     }
     public void TakeDamage(int damage)
     {
-        if (!isInvincible)
+        currentHealth -= damage;
+        Destroy(coracoes[currentHealth]);
+        if (currentHealth <= 0)
         {
-            currentHealth -= damage;
-            Destroy(coracoes[currentHealth]);
-            isInvincible = true;
-            if (currentHealth <= 0)
-            {
-                Destroy(gameObject);
-                deathScreen.SetActive(true);
-            }
+            Destroy(gameObject);
         }
     }
     void RotateTowardsMouse()
@@ -147,15 +127,4 @@ public class Player : MonoBehaviour
         }
         isDashing = false;
     }
-
-    void UpdateInvencibility()
-    {
-        invincibleCurrentTime += Time.deltaTime * 1.0f; // Incrementa o tempo de invencibilidade
-        if (invincibleCurrentTime >= invincibleTime)
-        {
-            isInvincible = false;
-            invincibleCurrentTime = 0;
-        }
-    }
 }
-
