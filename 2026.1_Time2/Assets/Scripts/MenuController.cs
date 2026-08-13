5a2c326f0aac2b769d00a729b1d810b382fc11c2
Cacuito <kezen2005@hotmail.com>
2026-08-13 20:42:39 -0300

    Menu Finalizado eu acho

--- 2026.1_Time2/Assets/Scripts/MenuController.cs
+++ 2026.1_Time2/Assets/Scripts/MenuController.cs
@@ -19,34 +19,65 @@ public class MenuController : MonoBehaviour
 
     public void StartGame()
     {
+        TocarSomBotao();
         SceneManager.LoadScene("RoadMap");
     }
 
     public void OpenSettings()
     {
+        TocarSomBotao();
         Menu.SetActive(false);
         Config.SetActive(true);
     }
 
     public void BackToMenu()
     {
+        TocarSomBotao();
         Config.SetActive(false);
         Menu.SetActive(true);
     }
     
     public void QuitGame()
     {
+        TocarSomBotao();
         Application.Quit();
     }
 
     public void OpenControls()
     {
+        TocarSomBotao();
         Config.SetActive(false);
         Controles.SetActive(true);
     }
     public void BackToSettings()
     {
+        TocarSomBotao();
         Controles.SetActive(false);
         Config.SetActive(true);
+    }
+
+    // Funções para os Sliders de Áudio
+    public void MudarVolumeGeral(float valor)
+    {
+        if (AudioManager.Instance != null)
+            AudioManager.Instance.SetVolumeGeral(valor);
+    }
+
+    public void MudarVolumeMusica(float valor)
+    {
+        if (AudioManager.Instance != null)
+            AudioManager.Instance.SetVolumeMusica(valor);
+    }
+
+    public void MudarVolumeSFX(float valor)
+    {
+        if (AudioManager.Instance != null)
+            AudioManager.Instance.SetVolumeSFX(valor);
+    }
+
+    private void TocarSomBotao()
+    {
+        if (AudioManager.Instance != null)
+            AudioManager.Instance.PlayCliqueBotao();
     }
 }
